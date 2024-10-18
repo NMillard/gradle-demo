8b98ca8369026363d7ff78b001b8f4968001d1f6
Nicklas Millard <n.millard@me.com>
2024-10-19 00:53:06 +0200

    use latest release

--- persistence/build.gradle.kts
+++ persistence/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 dependencies {
     implementation(libs.logback)
-    implementation("ch.qos.logback:logback-core:${providers.gradleProperty("logback.version").get()}")
-    implementation("ch.qos.logback:logback-classic:${providers.gradleProperty("logback.version").get()}")
+    implementation("ch.qos.logback:logback-core:latest.release")
+    implementation("ch.qos.logback:logback-classic:latest.release")
     implementation("org.slf4j:slf4j-api:${providers.gradleProperty("slf4j.version")}")
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
