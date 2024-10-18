db400d1cf2ec9752aa610edf060de8df1c883881
Nicklas Millard <n.millard@me.com>
2024-10-19 00:50:27 +0200

    add deps to persistence lib

--- persistence/build.gradle.kts
+++ persistence/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
-    java
+    `java-library`
+}
+
+dependencies {
+    implementation(libs.logback)
+    implementation("ch.qos.logback:logback-core:${providers.gradleProperty("logback.version").get()}")
+    implementation("ch.qos.logback:logback-classic:${providers.gradleProperty("logback.version").get()}")
+    implementation("org.slf4j:slf4j-api:${providers.gradleProperty("slf4j.version")}")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
--- persistence/src/main/java/com/mjukvare/demos/persistence/InMemoryUserRepository.java
+++ persistence/src/main/java/com/mjukvare/demos/persistence/InMemoryUserRepository.java
@@ -1,8 +1,13 @@
 package com.mjukvare.demos.persistence;
 
+ import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class InMemoryUserRepository implements UserRepository {
     @Override
     public String getUsername() {
+        log.info("Getting username");
+
         return "hello";
     }
 }
